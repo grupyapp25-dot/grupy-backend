6b9e642f148b40eb34c47c6aba11824156eeac55
grupyapp25 <grupyapp25@gmail.com>
2025-09-21 15:01:04 +0200

    Update mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,30 +1,32 @@
-// utils/mailer.js
-import nodemailer from 'nodemailer';
+// utils/mailer.js (CommonJS)
+const nodemailer = require('nodemailer');
 
 const {
   SMTP_HOST,
-  SMTP_PORT,
+  SMTP_PORT = '587',
   SMTP_USER,
   SMTP_PASS,
   MAIL_FROM,
 } = process.env;
 
-export const transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
   host: SMTP_HOST,
-  port: Number(SMTP_PORT || 587),
-  secure: Number(SMTP_PORT) === 465, // true se usi 465
+  port: Number(SMTP_PORT),
+  secure: Number(SMTP_PORT) === 465, // true su 465
   auth: {
     user: SMTP_USER,
     pass: SMTP_PASS,
   },
 });
 
-export async function sendMail({ to, subject, html, text }) {
+async function sendMail({ to, subject, html, text }) {
   return transporter.sendMail({
     from: MAIL_FROM || SMTP_USER,
     to,
     subject,
-    text,
     html,
+    text,
   });
 }
+
+module.exports = { transporter, sendMail };
